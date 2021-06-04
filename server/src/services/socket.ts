ee1f8fb81b4f0a844a9eb91cc90bc5b326fcf861
Nitigya Kapoor <kapoornitigya@gmail.com>
2021-06-04 18:03:18 +0530

    container on demand

--- server/src/services/socket.ts
+++ server/src/services/socket.ts
@@ -9,7 +9,7 @@ import stderr from "../stream";
 const StringDecoder = require('string_decoder').StringDecoder;
 const decoder = new StringDecoder('utf8');
 
-// one class instance therefore only one user can issue commands for now
+// one instance therefore only one user can issue commands for now
 // for scaling use socket-cluster
 let exp : any = {};
 let socket : any;
@@ -26,8 +26,8 @@ exp.attachServer = (server) => {
     io.on("connection", async (sck) => {
         console.log("client connect to socket ", sck.id);
 
-        // const id = await Container.startNewRustContainer();
-        // console.log("started new container ", id )
+        const id = await Container.startNewRustContainer();
+        console.log("started new container ", id )
 
         socket = sck;
 
@@ -40,8 +40,7 @@ exp.attachServer = (server) => {
         socket.on("input", (input) => {
 
             // validate input 
-          //  Container.runCommand("f5affc6470296035e8c17a89f3883cc92d30e7282b6b6e4162a3951f660a1e77", input)
-            Container.runCommand("0706abcd6f613eb9f2f87021435bd68fd929688112e7b7610be2f9c63a83b3ec", input)
+            Container.runCommand(id, input)
 
             // wait for container run command
             stdout.on('data', chunk => {
@@ -60,8 +59,7 @@ exp.attachServer = (server) => {
         socket.on("input-code", async (input) => {
 
             // validate input 
-          //  Container.runCommand("f5affc6470296035e8c17a89f3883cc92d30e7282b6b6e4162a3951f660a1e77", input)
-            await Container.runCode("0706abcd6f613eb9f2f87021435bd68fd929688112e7b7610be2f9c63a83b3ec", input)
+            Container.runCode(id, input)
 
             // wait for container run command
             stdout.on('data', chunk => {
